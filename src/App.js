fd38b7ad1a547a7806e230b46c0fdca161423d41
Alina Gasperino <alinacg207@gmail.com>
2021-11-12 16:30:25 -0500

    Table is fully functional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import Table from './Table';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-       
-      </header>
+      <>
+      <Table/>
+      </>
     </div>
   );
 }
