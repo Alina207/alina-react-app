74f4d6475167336766e0b7d9cd9c4e1475bff337
Alina Gasperino <alinacg207@gmail.com>
2021-11-11 22:37:11 -0500

    Removed comments

--- src/App.js
+++ src/App.js
@@ -6,36 +6,24 @@ import data from "./mock-data.json";
 const App = () => {
   
   const [contacts, setContacts] = useState(data);
-// Store the form values as an object and state in a single state hook
-// The properties below match the name attribute on the form inputs
   const [addFormData, setAddFormData] = useState({
     fullName: '',
     address: '',
     phoneNumber: '',
     email: ''  
   })
-// Use an event handler to update the values
+
   const handleAddFormChange = (event) => {
     event.preventDefault();
-// First get the name attribute of the input the user has changed
     const fieldName = event.target.getAttribute('name');
-// Get the actual value the user entered in the input
     const fieldValue = event.target.value;
-// Make a copy of the existing form data so we can change it without mutating the state
-    // We're using the spread operator to copy the existing form data and assign the new data to that variable
     const newFormData = { ...addFormData};
-// Update the object with the new value the user has typed
-    // Remember newFormData is an object so we can use the [] to get a key
     newFormData[fieldName] = fieldValue;
-// Set it into state and pass in
     setAddFormData(newFormData)
   }
 
-// Add a function that gets called when the form is submitted
-// Take the data the use has entered which we have stored as addFormData in our useState() hook
-// and create a new object from it
   const handleAddFormSubmit = (event) => {
-    event.preventDefault(); // prevent post request
+    event.preventDefault(); 
 
     const newContact = {
       id: nanoid(),
